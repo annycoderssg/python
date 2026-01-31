fcca6d607a202f0fab31464d18b894dbb05779d1
247software-anand-shinde <anand.shinde@247software.com>
2026-01-31 14:46:53 +0530

    add-comment-validate

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -1,11 +1,29 @@
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+#   -	Addition
+#   -	Subtraction
+#   -	Multiplication
+#   -	Division
+# 3.  Displays the results of each operation on the screen.
+
 Add = lambda num1, num2 : num1 + num2
 Subtract = lambda num1, num2 : num1 - num2 if num1 > num2 else num2 - num1
 Multiply = lambda num1, num2 : num1 * num2
 Divide = lambda num1, num2 : num1 / num2 if num2 != 0 else "Invalid Input"
 
+def validateNumbers(num1, num2):
+    if num1 < 0 and num2 < 0:
+        return False
+    else:
+        return True
+
 def main():
     num1 = int(input("Enter the First Number: "))
     num2 = int(input("Enter the Second Number: "))
+
+    if not validateNumbers(num1, num2):
+        print("Number Should be Positive")
+        return
     
     addition = Add(num1, num2)
     print("Addition : ", addition)
--- Assignments/Assignment2.py
+++ Assignments/Assignment2.py
@@ -1,3 +1,8 @@
+# Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
 def main():
     firstName = input("Enter a your first name: ")
     lastName = input("Enter a your last name: ")
